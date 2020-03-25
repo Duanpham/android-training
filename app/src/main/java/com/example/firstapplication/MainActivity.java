fea8cf4fdd8f7a3e6ad8c5eaf55a0d7213ff177b
Duan Pham <duan@telesense.com>
2020-03-26 00:09:37 +0700

    Revert "05-06-07 review java"
    This reverts commit bfec5b6584f6fe18f311c9d57a218f00041bbe27.

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Switch;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -34,40 +32,5 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default: Log.d("DuanPham", "No Value");
         }
-
-        int age = 1;
-
-        while (age <10) {
-            age++;
-            Log.d("DuanPham " , String.valueOf(age));
-        }
-
-        for(int i = 0; i<10 ; i++) {
-            Log.d("DuanPham", String.valueOf(i));
-        }
-
-        ArrayList<String> arrayCity = new ArrayList<>();
-        arrayCity.add("HaNoi");
-        arrayCity.add("NhaTrang");
-        arrayCity.add("HoChiMinh");
-
-        Log.d("DuanPham", "Size: " + arrayCity.size());
-        Log.d("DuanPham", "First Element: " + arrayCity.get(0));
-
-        arrayCity.remove(0);
-        Log.d("DuanPham", "First Element: " + arrayCity.get(0));
-
-        for(String city: arrayCity) {
-            Log.d("DuanPham", city);
-        }
-
-
-        hello("Duan", 22);
-        hello("Nick", 23);
-        hello("Anna", 24);
-    }
-
-    private void hello(String name, int age) {
-        Log.d("DuanPham", "Bonjour! je m'applle " + name +",j'ai " + age + " ans");
     }
 }
